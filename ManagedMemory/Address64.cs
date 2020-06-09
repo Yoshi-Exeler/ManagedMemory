61a13ef4630f2f67155a2f1709e3fe89e4f6295a
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2020-06-09 15:36:24 +0200

    Implement Address64

---
+++ ManagedMemory/Address64.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedMemory
+{
+    class Address64
+    {
+        private long address;
+
+        public Address64(long adr)
+        {
+            address = adr;
+        }
+
+        public long getAddress()
+        {
+            return address;
+        }
+
+        public IntPtr getAddressAsPointer()
+        {
+            return (IntPtr)address;
+        }
+
+        public void setAddress(int adr)
+        {
+            address = adr;
+        }
+
+        public void setAddress(IntPtr adr)
+        {
+            address = (long)adr;
+        }
+    }
+}
