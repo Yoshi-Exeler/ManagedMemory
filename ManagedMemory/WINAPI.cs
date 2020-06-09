9ce5cef04a32ff8fb79f0f53829251568a596e39
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2020-06-09 14:13:45 +0200

    Imports from Kernel

--- ManagedMemory/WINAPI.cs
+++ ManagedMemory/WINAPI.cs
@@ -3,10 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
+using System.Diagnostics;
 
 namespace ManagedMemory
 {
     class WINAPI
     {
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 2035711u,
+            Terminate = 1u,
+            CreateThread = 2u,
+            VirtualMemoryOperation = 8u,
+            VirtualMemoryRead = 16u,
+            VirtualMemoryWrite = 32u,
+            DuplicateHandle = 64u,
+            CreateProcess = 128u,
+            SetQuota = 256u,
+            SetInformation = 512u,
+            QueryInformation = 1024u,
+            QueryLimitedInformation = 4096u,
+            Synchronize = 1048576u
+        }
+
+        [DllImport("NTDLL.DLL", SetLastError = true)]
+        public static extern int NtQueryInformationProcess(IntPtr hProcess, int pic, IntPtr pbi, uint cb, out uint pSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int dwSize, out UIntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, IntPtr nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        [Flags]
+        public enum MemoryProtection
+        {
+            Execute = 16,
+            ExecuteRead = 32,
+            ExecuteReadWrite = 64,
+            ExecuteWriteCopy = 128,
+            NoAccess = 1,
+            ReadOnly = 2,
+            ReadWrite = 4,
+            WriteCopy = 8,
+            GuardModifierflag = 256,
+            NoCacheModifierflag = 512,
+            WriteCombineModifierflag = 1024
+        }
+
+        [Flags]
+        public enum FreeType
+        {
+            Decommit = 16384,
+            Release = 32768
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool VirtualFree(IntPtr lpAddress, int dwSize, FreeType dwFreeType);
+
     }
 }
