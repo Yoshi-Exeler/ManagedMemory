9b4979766bd959bf7790b09a92131729e107ce92
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2020-06-09 16:09:14 +0200

    Implement Pointer Class

--- ManagedMemory/Pointer.cs
+++ ManagedMemory/Pointer.cs
@@ -8,5 +8,34 @@ namespace ManagedMemory
 {
     class Pointer
     {
+        private Address64 source;
+        private Address64 destination;
+        private ProcessInterface callback;
+
+        public Pointer(Address64 src, ProcessInterface callback)
+        {
+            source = src;
+            destination = new Address64(callback.ReadInt64(source));
+        }
+
+        public Address64 getSource()
+        {
+            return source;
+        }
+
+        public Address64 getDestination()
+        {
+            return destination;
+        }
+
+        public void update()
+        {
+            destination = new Address64(callback.ReadInt64(source));
+        }
+
+        public ExternalVariable getDestinationVariable()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
