3f8b8a685d48e5a74362e6c45ae50301bb9a2959
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2020-06-13 15:35:53 +0200

    implement handle class

---
+++ ManagedMemory/Handle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedMemory
+{
+    public class Handle
+    {
+        private long handle;
+        private ProcessInterface callback;
+
+        public Handle(long handle, ProcessInterface callback)
+        {
+            this.handle = handle;
+            this.callback = callback;
+        }
+
+        public Handle(IntPtr handle, ProcessInterface callback)
+        {
+            this.handle = (long)handle;
+            this.callback = callback;
+        }
+
+        public long GetHandleAsLong()
+        {
+            return handle;
+        }
+
+        public IntPtr GetHandleAsPointer()
+        {
+            return (IntPtr)handle;
+        }
+
+        public void Close()
+        {
+            APIProxy.CloseHandle((IntPtr)handle);
+        }
+
+    }
+}
